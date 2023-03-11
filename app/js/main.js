32778a8421e18ed131a142202aa14b772edd099a
slavik <slavik.zaliotnyi@gmail.com>
2023-03-12 01:52:12 +0200

    added header and historu section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
 $(function () {
+  const menuList = document.querySelector(".menu__list");
+  const body = document.querySelector("body");
+  const openMenu = document.querySelector(".menu__btn");
+  const closeMenu = document.querySelector(".menu__close");
 
-})
+  openMenu.addEventListener("click", () => {
+    if (!menuList.classList.contains("open")) {
+      menuList.classList.add("open");
+      body.classList.add('hidden');
+    } else {
+      menuList.classList.remove("open");
+      body.classList.remove("hidden");
+    }
+  });
+
+  closeMenu.addEventListener("click", () => {
+    if (menuList.classList.contains("open")) {
+      menuList.classList.remove("open");
+       body.classList.remove("hidden");
+    } else {
+      menuList.classList.add("open");
+       body.classList.add("hidden");
+    }
+  });
+});
